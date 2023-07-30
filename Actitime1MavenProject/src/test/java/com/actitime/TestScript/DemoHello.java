a17e6682b736e2a15bbe1b91d99de26d106e5a51
T. Karthik <T. Karthik@192.168.1.5>
2023-07-30 13:14:18 +0530

    commit of Actitime1MavenProject

---
+++ Actitime1MavenProject/src/test/java/com/actitime/TestScript/DemoHello.java
@@ -0,0 +1,11 @@
+package com.actitime.TestScript;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoHello {
+	@Test
+	public void testHelloworld() {
+		Reporter.log("---------Hello Bangalore----------",true);
+	}
+
+}
